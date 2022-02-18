c4b1047a127511485a32f23de37bcd50e4f2c2cf
Mikael Briolet <mbriolet.ma@gmail.com>
2022-02-18 10:56:55 +0100

    add github action for jest
    + add jest task
    + use pty for tasks

--- back/test_index.py
+++ back/test_index.py
@@ -8,6 +8,7 @@ client = TestClient(app)
 
 
 def test_server_is_alive():
+    """Home page returns HTML content."""
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"status": "alive"}
+    assert "<!doctype html>" in str(response.content)
--- tasks.py
+++ tasks.py
@@ -8,24 +8,29 @@ from invoke import task
 @task
 def build(command):
     """Build the containers."""
-    command.run("docker-compose build")
+    command.run("docker-compose build", pty=True)
 
 @task
 def up(command):
     """Up the containers."""
-    command.run("docker-compose up -d")
+    command.run("docker-compose up -d", pty=True)
 
 @task
 def down(command):
     """Stop and down the containers."""
-    command.run("docker-compose down")
+    command.run("docker-compose down", pty=True)
 
 @task
 def pytest(command):
     """Run the python tests inside the web container."""
-    command.run("docker-compose run --rm web python3 -m pytest")
+    command.run("docker-compose run --rm web python3 -m pytest", pty=True)
+
+@task
+def jest(command):
+    """Run the Jest tests inside the vue container."""
+    command.run("docker-compose run --rm vue yarn run jest", pty=True)
 
 @task
 def vuesh(command):
     """Create a bash terminal in the vue container."""
-    command.run("docker-compose run --rm vue /bin/sh")
+    command.run("docker-compose run --rm vue /bin/sh", pty=True)
