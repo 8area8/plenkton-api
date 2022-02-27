656b574a24f3ca3aab50d350799ab4ecf126208d
Mikael Briolet <mbriolet.ma@gmail.com>
2022-02-27 20:18:15 +0000

    add admin page
    + very basic admin page

---
+++ front/src/views/Admin.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="admin">
+      This is the admin dashboard (supercharge the home dashboard with custom slots)
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeView",
+});
+</script>
